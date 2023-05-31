033d30a76349fafa4c9442754cc803140c3d7c14
NatanBondarenko <97616282+NatanBondarenko@users.noreply.github.com>
2023-05-31 18:23:14 +0300

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -3,13 +3,25 @@ looker.plugins.visualizations.add({
     url: {
       type: 'string',
       label: 'URL',
-      default: 'https://www.google.com'
+      default: 'https://www.google.com/search?igu=1'
+    },
+    height: {
+      type: 'string',
+      label: 'Height',
+      default: '400px'
+    },
+    width: {
+      type: 'string',
+      label: 'Width',
+      default: '600px'
     }
   },
   create: function(element, config) {
     // Create the iframe element
     const iframe = document.createElement('iframe');
     iframe.src = config.url;
+    iframe.style.height = config.height;
+    iframe.style.width = config.width;
 
     // Append the iframe to the container element
     element.appendChild(iframe);
