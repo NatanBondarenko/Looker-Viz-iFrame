2eb584af02e24d05b6737a3c0431b38a40f2c381
NatanBondarenko <97616282+NatanBondarenko@users.noreply.github.com>
2023-05-31 18:03:47 +0300

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,11 +1,11 @@
 looker.plugins.visualizations.add({
-options: {
-        url: {
+  options: {
+    url: {
       type: 'string',
       label: 'URL',
-      default: '{{ value }}'
-        }
-    },
+      default: 'https://www.google.com'
+    }
+  },
   create: function(element, config) {
     // Create the iframe element
     const iframe = document.createElement('iframe');
@@ -14,7 +14,8 @@ options: {
     // Append the iframe to the container element
     element.appendChild(iframe);
   },
-   updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
+  updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
     // No data manipulation or updates needed for this visualization
+    doneRendering();
   }
 });
