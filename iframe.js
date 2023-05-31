3799024c20eeeea472c34c97b8280db6542c88af
NatanBondarenko <97616282+NatanBondarenko@users.noreply.github.com>
2023-05-31 16:29:17 +0300

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,22 +1,24 @@
 looker.plugins.visualizations.add('iframe_viz', {
   options: {
-    url: {
+    html_template: {
       type: 'string',
       label: 'URL',
       default: '',
+      display: flex,
       order: 1
     },
     description: {
       type: 'string',
       label: 'Description',
       default: '',
+      display: flex,
       order: 2
     }
   },
   create: function(element, config) {
     // Create the iframe element
     const iframe = document.createElement('iframe');
-    iframe.src = config.url;
+    iframe.src = config.html_template;
     iframe.title = config.description;
 
     // Append the iframe to the container element
